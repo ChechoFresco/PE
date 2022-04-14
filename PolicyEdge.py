abdc228247c37416e2dc8f4a0d329e4e09220943
Sergio Preciado <79943465+ChechoFresco@users.noreply.github.com>
2022-04-13 17:18:40 -0700

    Update PolicyEdge.py

--- PolicyEdge.py
+++ PolicyEdge.py
@@ -651,7 +651,7 @@ def savedIssues():
                             Date.append(start_month+'/'+start_day+'/'+start_year)
                             meeting_type.append(i['MeetingType'])
                             item_type.append(i['ItemType'])
-                        flash(issues_placeholder[0])
+                        flash(issues_placeholder[0]['City']['searchWord'])
                 return render_template('savedIssues.html', form=form, agendas=agenda,  title='Monitor List')
 
             elif request.method == 'POST' and request.form['action'] == 'Add':
