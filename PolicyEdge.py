1d1df6dc87c832089fa4ebc703bb656807cb7d29
Sergio Preciado <79943465+ChechoFresco@users.noreply.github.com>
2022-04-25 17:45:44 -0700

    Update PolicyEdge.py

--- PolicyEdge.py
+++ PolicyEdge.py
@@ -107,7 +107,7 @@ def check4Issues2email():
             mail.send(msg)
 
 sched = BackgroundScheduler(timezone='UTC')
-sched.add_job(check4Issues2email, 'interval', seconds=3600)
+sched.add_job(check4Issues2email, 'interval', seconds=30)
 sched.start()
 
 @app.route('/', methods=['GET', 'POST'])
