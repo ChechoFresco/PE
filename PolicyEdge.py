602522c0b5548eeaa5c59c544950eb2b4f25a16b
Sergio Preciado <79943465+ChechoFresco@users.noreply.github.com>
2023-04-19 16:31:42 -0700

    Update PolicyEdge.py

--- PolicyEdge.py
+++ PolicyEdge.py
@@ -272,7 +272,7 @@ def index():
                 stat.append([Multiquery, cityList[z]] )
         if request.method == 'GET':
             agendaa = mongo.db.Agenda.find({'$and':[ {'$text': { "$search": chosen}}, {"MeetingType":" City Council "}, { 'Date':{'$lte':twoweekAhead, '$gte':threemonthBefore}}]}).sort('Date').sort('City')
-            return render_template('index.html',stats=stat,fdist1s=fdist1,fdist2s=fdist2, fdist3s=fdist3, agendaas=agendaa,chosen=chosen,chosencountyList=chosencountyList, form=form, form2=form2, title="Welcome to Policy Edge")
+            return render_template('index.html',stats=stat,fdist1s=fdist1,fdist2s=fdist2, fdist3s=fdist3,chosencountyList=chosencountyList, form=form, form2=form2, title="Welcome to Policy Edge")
         elif request.method == 'POST' and request.form['select'] == 'cannabis':
             chosen= 'cannabis'
             agendaa = mongo.db.Agenda.find({'$and':[ {'$text': { "$search": chosen}}, { 'Date':{'$lte':twoweekAhead, '$gte':threemonthBefore}}]}).sort('Date').sort('City')
