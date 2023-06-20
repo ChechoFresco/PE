4ccb44743b69ba1607c8cc00b6b31b0bfb34b5c3
Sergio Preciado <79943465+ChechoFresco@users.noreply.github.com>
2023-06-20 16:25:00 -0700

    Update PolicyEdge.py

--- PolicyEdge.py
+++ PolicyEdge.py
@@ -1,6 +1,6 @@
 from flask_pymongo import PyMongo
 from flask import Flask, render_template, url_for, request, redirect, flash, session, jsonify, json
-from forms import searchForm, monitorListform, newIssue, newTrend
+from forms import searchForm, monitorListform, newTrend
 import bcrypt
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
