2ba2dfd1d81eb15f229ff11d3c3beb1568d886e4
Sergio Preciado <79943465+ChechoFresco@users.noreply.github.com>
2023-06-27 17:57:05 -0700

    Update PolicyEdge.py

--- PolicyEdge.py
+++ PolicyEdge.py
@@ -1551,7 +1551,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_error(error):
-    return "500 error"
+    return render_template('500.html'), 500
     
 if __name__ == '__main__':
     app.run(debug = True)
