3826672da798596fe78256f397a04d796f31fedc
Sergio Preciado <79943465+ChechoFresco@users.noreply.github.com>
2026-03-09 14:30:22 -0700

    Update PolicyEdge.py

--- PolicyEdge.py
+++ PolicyEdge.py
@@ -162,56 +162,6 @@ def get_date_threshold(weeks=-2):
     """Get date threshold in YYYYMMDD format for database queries"""
     return int((date.today() + relativedelta(weeks=weeks)).strftime('%Y%m%d'))
 
-def fetch_geo_info(city_issue_counts):
-    """Fetch geo-location data from MongoDB for map generation"""
-    geo_info = []
-    for city, count in city_issue_counts.items():
-        location_data = mongo.db.geoLoc.find_one({'city': city}, {'_id': 0})
-        if location_data:
-            geo_info.append((
-                location_data['city'], 
-                location_data['state_id'], 
-                location_data['county_name'],
-                location_data['lat'], 
-                location_data['lng'], 
-                str(count), 
-                location_data['webAdress']
-            ))
-    return geo_info
-
-def create_folium_map(geo_info):
-    """Create a Folium map with circles and markers for agenda visualization"""
-    folium_map = folium.Map(
-        location=(34, -118), 
-        zoom_start=9, 
-        tiles="cartodbpositron", 
-        width=1000, 
-        height=475
-    )
-    
-    for city, state_id, county_name, lat, lon, issue_count, web_address in geo_info:
-        # Add circle marker proportional to issue count
-        folium.Circle(
-            location=[lat, lon],
-            popup=f"<a href='{web_address}' target='_blank'>{city} Agenda Link</a>",
-            radius=float(issue_count) * 50,
-            color='#5e7cff',
-            fill=True,
-            fill_color='#5e7cff'
-        ).add_to(folium_map)
-        
-        # Add text marker with count
-        folium.Marker(
-            location=[lat, lon],
-            icon=DivIcon(
-                icon_size=(10, 10),
-                icon_anchor=(15, 15),
-                html=f'<div style="font-size: 10pt">{issue_count} {city}</div>'
-            )
-        ).add_to(folium_map)
-    
-    return folium_map
-
 def validate_registration(username, email, password1, password2):
     """Validate registration form data and return list of errors"""
     errors = []
@@ -665,7 +615,7 @@ def index():
     city_issue_counts = Counter(cities_matched)
 
     # Get geo info from Mongo
-    geo_info = fetch_geo_info(city_issue_counts)
+    geo_info = fetch_geo_info(mongo, city_issue_counts)
 
     # Build Folium map
     folium_map = create_folium_map(geo_info)
